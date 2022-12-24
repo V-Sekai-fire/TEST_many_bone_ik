56e7acefab596f9a524771037b1175df73d7fe90
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-12-24 13:44:58 -0800

    Update bone constrain.

--- n_bone_ik/bone_constrain.gd
+++ n_bone_ik/bone_constrain.gd
@@ -18,7 +18,8 @@ func _run():
 	root.add_child(new_ik, true)
 	new_ik.skeleton_node_path = "../" + str(root.get_path_to(skeleton))
 	new_ik.owner = root
-
+	new_ik.iterations_per_frame = 120
+	new_ik.default_damp = deg_to_rad(20)
 	new_ik.visible = false
 	new_ik.constraint_mode = false
 	skeleton.reset_bone_poses()
@@ -39,10 +40,10 @@ func _run():
 		new_ik.set_pin_weight(bone_i, 0)
 	for bone_i in skeleton.get_bone_count():
 		var bone_name : String = skeleton.get_bone_name(bone_i)
+		new_ik.set_pin_passthrough_factor(bone_i, 0.8)
 		if bone_name in ["Root"]:
 			new_ik.set_pin_enabled(bone_i, true)
 			new_ik.set_pin_weight(bone_i, 1)
-			new_ik.set_pin_passthrough_factor(bone_i, 0.8)
 			new_ik.set_pin_nodepath(bone_i, ".")
 		if is_humanoid:
 			if bone_name in ["LeftToes", "RightToes"]:
@@ -55,7 +56,7 @@ func _run():
 			if not humanoid_bones.has(bone_name):
 				new_ik.set_pin_weight(bone_i, 0)
 				continue
-			if not bone_name in ["Head", "LeftFoot", "RightFoot"]: # "LeftHand", "RightHand", 
+			if not bone_name in ["Head", "LeftFoot", "RightFoot", "LeftHand", "RightHand",]:
 				continue
 			if bone_name in ["LeftFoot", "RightFoot"]:
 				new_ik.set_pin_passthrough_factor(bone_i, 0)
@@ -89,19 +90,19 @@ func _run():
 		"Head": Vector2(deg_to_rad(0), deg_to_rad(10)),
 		"Neck": Vector2(deg_to_rad(356), deg_to_rad(10)),
 		# Arms
-		"RightShoulder": Vector2(deg_to_rad(77), deg_to_rad(10)),
-		"LeftShoulder": Vector2(deg_to_rad(250), deg_to_rad(10)),
-		"RightUpperArm": Vector2(deg_to_rad(77), deg_to_rad(20)),
-		"LeftUpperArm": Vector2(deg_to_rad(250), deg_to_rad(20)),
-		"RightLowerArm": Vector2(deg_to_rad(77), deg_to_rad(20)),
-		"LeftLowerArm": Vector2(deg_to_rad(250), deg_to_rad(20)),
-		"RightHand": Vector2(deg_to_rad(281), deg_to_rad(10)),
-		"LeftHand": Vector2(deg_to_rad(78.6), deg_to_rad(10)),
+#		"RightShoulder": Vector2(deg_to_rad(77), deg_to_rad(10)),
+#		"LeftShoulder": Vector2(deg_to_rad(250), deg_to_rad(10)),
+#		"RightUpperArm": Vector2(deg_to_rad(77), deg_to_rad(20)),
+#		"LeftUpperArm": Vector2(deg_to_rad(250), deg_to_rad(20)),
+#		"RightLowerArm": Vector2(deg_to_rad(77), deg_to_rad(20)),
+#		"LeftLowerArm": Vector2(deg_to_rad(250), deg_to_rad(20)),
+#		"RightHand": Vector2(deg_to_rad(281), deg_to_rad(10)),
+#		"LeftHand": Vector2(deg_to_rad(78.6), deg_to_rad(10)),
 		# Legs
 		"LeftUpperLeg": Vector2(deg_to_rad(0), deg_to_rad(358)),
 		"RightUpperLeg": Vector2(deg_to_rad(0), deg_to_rad(358)),
-		"LeftLowerLeg": Vector2(deg_to_rad(180), deg_to_rad(-1)),
-		"RightLowerLeg": Vector2(deg_to_rad(180), deg_to_rad(1)),
+		"LeftLowerLeg": Vector2(deg_to_rad(180), deg_to_rad(-2)),
+		"RightLowerLeg": Vector2(deg_to_rad(180), deg_to_rad(2)),
 		"LeftFoot": Vector2(deg_to_rad(180), deg_to_rad(350)),
 		"RightFoot": Vector2(deg_to_rad(180), deg_to_rad(350)),
 		"LeftToes": Vector2(deg_to_rad(180), deg_to_rad(30)),
@@ -117,19 +118,19 @@ func _run():
 
 	var bone_name_cones : Dictionary = {
 		"Head": [{"center": Vector3(0, 1, 0)}],
-		"Root": [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(1)}],
+		"Root": [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(2)}],
 		"Neck": [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
 		"UpperChest": [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
 		"Chest": [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
 		"Spine": [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
-		"RightShoulder": [{"center": Vector3(-1, 0, 0), "radius": deg_to_rad(90)}],
-		"LeftShoulder": [{"center": Vector3(1, 0, 0), "radius": deg_to_rad(90)}],
-		"LeftUpperArm":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(90)}],
-		"RightUpperArm":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(90)}],
-		"LeftLowerArm":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
-		"RightLowerArm":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
-		"LeftHand":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
-		"RightHand":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
+#		"RightShoulder": [{"center": Vector3(-1, 0, 0), "radius": deg_to_rad(90)}],
+#		"LeftShoulder": [{"center": Vector3(1, 0, 0), "radius": deg_to_rad(90)}],
+#		"LeftUpperArm":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(90)}],
+#		"RightUpperArm":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(90)}],
+#		"LeftLowerArm":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
+#		"RightLowerArm":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
+#		"LeftHand":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
+#		"RightHand":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
 		"LeftUpperLeg":  [{"center": Vector3(0, -1, 0), "radius": deg_to_rad(80)}],
 		"RightUpperLeg":  [{"center": Vector3(0, -1, 0), "radius": deg_to_rad(80)}],
 		"LeftLowerLeg":  [
