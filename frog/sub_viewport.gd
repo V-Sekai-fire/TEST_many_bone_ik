66eb0be763f72ab097038670beb452e0e8abf435
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-12-27 21:18:16 -0800

    Show lasso system.

--- frog/sub_viewport.gd
+++ frog/sub_viewport.gd
@@ -10,19 +10,26 @@ func _ready():
 	var hips : Node3D = $SubViewportContainer/SubViewport/Node3D/Frogdude/Frogdude2/RootNode/GeneralSkeleton/ManyBoneIK3D/Hips
 	var head : Node3D = $SubViewportContainer/SubViewport/Node3D/Frogdude/Frogdude2/RootNode/GeneralSkeleton/ManyBoneIK3D/Head
 	var left_hand : Node3D = $SubViewportContainer/SubViewport/Node3D/Frogdude/Frogdude2/RootNode/GeneralSkeleton/ManyBoneIK3D/LeftHand
+	var left_elbow : Node3D = $SubViewportContainer/SubViewport/Node3D/Frogdude/Frogdude2/RootNode/GeneralSkeleton/ManyBoneIK3D/LeftLowerArm
 	var right_hand : Node3D = $SubViewportContainer/SubViewport/Node3D/Frogdude/Frogdude2/RootNode/GeneralSkeleton/ManyBoneIK3D/RightHand
+	var right_elbow : Node3D = $SubViewportContainer/SubViewport/Node3D/Frogdude/Frogdude2/RootNode/GeneralSkeleton/ManyBoneIK3D/RightLowerArm
 	var left_foot : Node3D = $SubViewportContainer/SubViewport/Node3D/Frogdude/Frogdude2/RootNode/GeneralSkeleton/ManyBoneIK3D/LeftFoot
+	var left_knee : Node3D = $SubViewportContainer/SubViewport/Node3D/Frogdude/Frogdude2/RootNode/GeneralSkeleton/ManyBoneIK3D/LeftLowerLeg
 	var right_foot : Node3D = $SubViewportContainer/SubViewport/Node3D/Frogdude/Frogdude2/RootNode/GeneralSkeleton/ManyBoneIK3D/RightFoot
-	points.append_array([hips, head, left_hand, right_hand, left_foot, right_foot])
+	var right_knee : Node3D = $SubViewportContainer/SubViewport/Node3D/Frogdude/Frogdude2/RootNode/GeneralSkeleton/ManyBoneIK3D/RightLowerLeg
+	points.append_array([hips, head, left_hand, right_hand, left_foot, right_foot, left_elbow, right_elbow, left_knee, right_knee])
 
 	for point in points:
+		for child in point.get_children():
+			child.visible = false
 		var lasso_point : LassoPoint = LassoPoint.new()
-		point.lasso_point = lasso_point
 		lasso_point.register_point(lasso_db, point)
 
 var dragging = false
 
 func _input(event):	
+	var pointer : Node3D = $SubViewportContainer/SubViewport/Pointer
+	var pointer_secondary : Node3D = $SubViewportContainer/SubViewport/PointerSecondary
 	if event is InputEventMouseButton:
 		if event.button_mask == MOUSE_BUTTON_LEFT and event.pressed:
 			dragging = true
@@ -31,16 +38,18 @@ func _input(event):
 
 	if event is InputEventMouseMotion and event.button_mask == MOUSE_BUTTON_LEFT and dragging:
 		var motion = event.relative * 0.0008
-		var pointer : Node3D = $SubViewportContainer/SubViewport/Pointer
 		pointer.global_transform.origin = pointer.global_transform.origin + Vector3(motion.x, -motion.y, 0.0)
-
-	if event is InputEventMouseMotion and event.button_mask == MOUSE_BUTTON_RIGHT and dragging:
-		var motion = event.relative * 0.0005
-		var pointer : Node3D = $SubViewportContainer/SubViewport/Pointer
+	elif event is InputEventMouseMotion and event.button_mask == MOUSE_BUTTON_RIGHT and dragging:
+		var motion = event.relative * 0.0008
 		var viewport : Transform3D = $SubViewportContainer/SubViewport/Camera3D.get_camera_transform()
-		var snapped_nodes : Array = lasso_db.calc_top_two_snapping_power(pointer.global_transform, pointer, 0, 0, false)
-		for snapped_point in snapped_nodes:
-			var snapped_node = snapped_point.get_origin()
+		var snapped_nodes : Array = lasso_db.calc_top_two_snapping_power(pointer.global_transform, pointer, 1, 1, true)
+		var pointers = [pointer, pointer_secondary]
+		for snapped_point_i in range(snapped_nodes.size()):
+			var snapped_node = snapped_nodes[snapped_point_i].get_origin()
 			$SubViewportContainer/SubViewport/Label3D.text = snapped_node.get_name()
-			snapped_node.global_transform.origin = snapped_node.global_transform.origin + Vector3(motion.x, -motion.y, 0.0)
-			break
+			var snapped_transform : Transform3D = Transform3D()
+			snapped_transform = snapped_node.global_transform
+			snapped_transform.origin = snapped_transform.origin + Vector3(motion.x, -motion.y, 0.0)
+			snapped_node.global_transform = snapped_transform
+			if snapped_point_i == 1:
+				pointers[snapped_point_i].global_transform = snapped_node.global_transform
--- n_bone_ik/bone_constrain.gd
+++ n_bone_ik/bone_constrain.gd
@@ -53,7 +53,7 @@ func _run():
 				new_ik.set_pin_weight(bone_i, 1)
 			if bone_name in ["LeftFoot", "RightFoot"]:
 				new_ik.set_pin_passthrough_factor(bone_i, 0)
-			if not bone_name in ["Hips", "Head", "LeftFoot", "RightFoot", "LeftHand", "RightHand",]:
+			if not bone_name in ["Hips", "Head", "LeftLowerLeg", "LeftFoot", "RightLowerLeg", "RightFoot", "LeftHand", "LeftLowerArm", "RightLowerArm", "RightHand",]:
 				continue
 		var node_3d : BoneAttachment3D = BoneAttachment3D.new()
 		node_3d.name = bone_name
