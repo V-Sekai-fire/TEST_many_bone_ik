8e11aa7015e00cf734e9b7ff85fefe8e5e7ee327
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-01-05 10:35:18 -0800

    Remove scripts.

--- n_bone_ik/bone_constrain.gd
+++ n_bone_ik/bone_constrain.gd
@@ -50,80 +50,6 @@ func _run():
 		humanoid_bones.push_back(bone_name)
 	var is_humanoid : bool = false
 	var is_filtering : bool = true
-
-#	var config =  {
-#		"bone_name_from_to_twist": {
-#			# Don't put constraints on the root bone.
-#			# Spine
-#			"Hips": Vector2(deg_to_rad(0), deg_to_rad(350)),
-#			"Spine": Vector2(deg_to_rad(355), deg_to_rad(30)),
-#			"Chest":  Vector2(deg_to_rad(355), deg_to_rad(30)),
-#			"UpperChest": Vector2(deg_to_rad(355), deg_to_rad(30)),
-#			# Head
-#			"Head": Vector2(deg_to_rad(0), deg_to_rad(10)),
-#			"Neck": Vector2(deg_to_rad(356), deg_to_rad(10)),
-#			# Arms
-#			"LeftShoulder": Vector2(deg_to_rad(-250), deg_to_rad(-40)),
-#			"RightShoulder": Vector2(deg_to_rad(250), deg_to_rad(-40)),
-#			"LeftUpperArm": Vector2(deg_to_rad(-120), deg_to_rad(-60)),
-#			"RightUpperArm": Vector2(deg_to_rad(120), deg_to_rad(60)),
-#			"LeftLowerArm": Vector2(deg_to_rad(-75), deg_to_rad(-60)),
-#			"RightLowerArm": Vector2(deg_to_rad(75), deg_to_rad(60)),
-#			"LeftHand": Vector2(deg_to_rad(-275), deg_to_rad(-20)),
-#			"RightHand": Vector2(deg_to_rad(275), deg_to_rad(20)),
-#			# Legs
-#			"LeftUpperLeg": Vector2(deg_to_rad(00), deg_to_rad(350)),
-#			"RightUpperLeg": Vector2(deg_to_rad(00), deg_to_rad(350)),
-#			"LeftLowerLeg": Vector2(deg_to_rad(90), deg_to_rad(20)),
-#			"RightLowerLeg": Vector2(deg_to_rad(90), deg_to_rad(20)),
-#			"LeftFoot": Vector2(deg_to_rad(180), deg_to_rad(350)),
-#			"RightFoot": Vector2(deg_to_rad(180), deg_to_rad(350)),
-#		},
-#		"bone_name_cones": {
-#			# Don't put constraints on the root bone.			
-#			"Hips": [{"center": Vector3(0, -1, 0), "radius": deg_to_rad(5)}],
-#			"Spine": [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(10)}],
-#			"UpperChest": [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(5)}],
-#			"Chest": [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(10)}],
-#			"Neck": [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(15)}],
-#			"Head": [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(15)}],
-#			"LeftShoulder": [{"center": Vector3(1, 0, 0), "radius": deg_to_rad(15)}],
-#			"RightShoulder": [{"center": Vector3(-1, 0, 0), "radius": deg_to_rad(15)}],
-#			"LeftUpperArm":  [
-#				{"center": Vector3(0, 1, -0.5), "radius": deg_to_rad(50)},
-#				{"center": Vector3(2, 0.5, -0.2), "radius": deg_to_rad(65)},
-#				{"center": Vector3(0, 0.2, 1), "radius": deg_to_rad(20)},
-#			],
-#			"RightUpperArm":  [
-#				{"center": Vector3(0, 1, -0.5), "radius": deg_to_rad(50)},
-#				{"center": Vector3(-2, 0.5, -0.2), "radius": deg_to_rad(65)},
-#				{"center": Vector3(0, 0.2, 1), "radius": deg_to_rad(20)},
-#			],
-#			"LeftLowerArm":  [
-#				{"center": Vector3(0, 0, 1), "radius": deg_to_rad(20)},
-#				{"center": Vector3(0, 0.8, 0), "radius": deg_to_rad(20)},
-#			],
-#			"RightLowerArm":  [
-#				{"center": Vector3(0, 0, 1), "radius": deg_to_rad(20)},
-#				{"center": Vector3(0, 0.8, 0), "radius": deg_to_rad(20)},
-#			],
-#			"LeftHand":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
-#			"RightHand":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
-#			# Legs
-#			"LeftUpperLeg":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(160)}],
-#			"RightUpperLeg":  [{"center": Vector3(0, -1, 0), "radius": deg_to_rad(160)}],
-#			"LeftLowerLeg":  [
-#				{"center": Vector3(0, 1, 0), "radius": deg_to_rad(10)},
-#				{"center": Vector3(0, -0.8, -1), "radius": deg_to_rad(40)},
-#			],
-#			"RightLowerLeg":  [
-#				{"center": Vector3(0, 1, 0), "radius": deg_to_rad(10)},
-#				{"center": Vector3(0, -0.8, -1), "radius": deg_to_rad(40)},
-#			],
-#			"LeftFoot":  [{"center": Vector3(1, 0, 0), "radius": deg_to_rad(90)}],
-#			"RightFoot":  [{"center": Vector3(1, 0, 0), "radius": deg_to_rad(90)}],
-#		},
-#	}
 	for bone_i in skeleton.get_bone_count():
 		var bone_name : String = skeleton.get_bone_name(bone_i)
 		if bone_name in humanoid_bones:
@@ -174,18 +100,6 @@ func tune_bone(new_ik : ManyBoneIK3D, skeleton, bone_name, bone_i):
 	node_3d.set_use_external_skeleton (true)
 	node_3d.set_external_skeleton("../" + str(new_ik.get_path_to(skeleton)))
 	new_ik.add_child(node_3d, true)
-#	if bone_name in ["Root"]:
-#		new_ik.set_pin_passthrough_factor(bone_i, 0)
-#	if bone_name in ["Head"]:
-#		# Move slightly higher to avoid the crunching into the body effect.
-##		node_3d.transform.origin = node_3d.transform.origin + Vector3(0, 0.1, 0)
-#		new_ik.set_pin_passthrough_factor(bone_i, 1)
-#	if bone_name in ["LeftHand"]:
-#		new_ik.set_pin_passthrough_factor(bone_i, 1)
-#	if bone_name in ["RightHand"]:
-#		new_ik.set_pin_passthrough_factor(bone_i, 1)
-#	if bone_name in ["LeftFoot", "RightFoot"]:
-#		new_ik.set_pin_passthrough_factor(bone_i, 1)
 	node_3d.owner = new_ik.owner
 	new_ik.set_pin_nodepath(bone_i, NodePath(bone_name))
 	var node_global_transform = node_3d.global_transform
