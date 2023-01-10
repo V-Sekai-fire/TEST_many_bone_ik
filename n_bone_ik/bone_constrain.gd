7daa8f5d7cb6dc5b79a723d82cf9d32c6e25c2a5
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-01-10 15:06:48 -0800

    For character posing. Update Black Cat Fire Christmas 2022.tscn, bone_constrain.gd, and many_bone_ik_test_rig.tscn

--- n_bone_ik/bone_constrain.gd
+++ n_bone_ik/bone_constrain.gd
@@ -9,7 +9,23 @@ extends EditorScript
 # 3. UpperChest to Hands
 # 4. Hips to Legs
 
-@export var targets : PackedStringArray = ["Root", "Head", "LeftFoot", "RightFoot", "LeftHand", "RightHand"]
+var is_humanoid : bool = true
+var is_filtering : bool = true
+	
+@export var targets : PackedStringArray = [
+#	"Root", 
+	"Hips", "Head", 
+#	"LeftLowerLeg", 
+	"LeftFoot", 
+#	"RightLowerLeg", 
+	"RightFoot", 
+#	"RightToes",
+#	"LeftLowerArm", 
+	"LeftHand",
+##	"RightLowerArm", 
+	"RightHand",
+#	"RightToes"
+	]
 @export	var config : Dictionary = {
 		"bone_name_from_to_twist": {
 			# Don't put constraints on the root bone.
@@ -35,14 +51,13 @@ extends EditorScript
 			"RightUpperLeg": Vector2(deg_to_rad(00), deg_to_rad(350)),
 			"LeftLowerLeg": Vector2(deg_to_rad(90), deg_to_rad(20)),
 			"RightLowerLeg": Vector2(deg_to_rad(90), deg_to_rad(20)),
-			"LeftFoot": Vector2(deg_to_rad(180), deg_to_rad(350)),
-			"RightFoot": Vector2(deg_to_rad(180), deg_to_rad(350)),
+#			"LeftFoot": Vector2(deg_to_rad(180), deg_to_rad(5)),
+#			"RightFoot": Vector2(deg_to_rad(180), deg_to_rad(5)),
 		},
 		"bone_name_cones": {
 			# Don't put constraints on the root bone.			
 			"Hips": [
 				{"center": Vector3(0, -1, 0), "radius": deg_to_rad(5)},
-				{"center": Vector3(0, 1, 0), "radius": deg_to_rad(5)}
 			],
 			"Spine": [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(10)}],
 			"UpperChest": [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(5)}],
@@ -72,18 +87,24 @@ extends EditorScript
 			"LeftHand":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
 			"RightHand":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(20)}],
 			# Legs
-			"LeftUpperLeg":  [{"center": Vector3(0, 1, 0), "radius": deg_to_rad(160)}],
-			"RightUpperLeg":  [{"center": Vector3(0, -1, 0), "radius": deg_to_rad(160)}],
+			"LeftUpperLeg":  [
+				{"center": Vector3(0, 1, 0), "radius": deg_to_rad(160)}
+			],
+			"RightUpperLeg":  [
+				{"center": Vector3(0, 1, 0), "radius": deg_to_rad(160)}
+			],
 			"LeftLowerLeg":  [
 				{"center": Vector3(0, 1, 0), "radius": deg_to_rad(10)},
-				{"center": Vector3(0, -0.8, -1), "radius": deg_to_rad(40)},
+				{"center": Vector3(0, -0.8, 1), "radius": deg_to_rad(40)},
 			],
 			"RightLowerLeg":  [
 				{"center": Vector3(0, 1, 0), "radius": deg_to_rad(10)},
-				{"center": Vector3(0, -0.8, -1), "radius": deg_to_rad(40)},
+				{"center": Vector3(0, -0.8, 1), "radius": deg_to_rad(40)},
 			],
-			"LeftFoot":  [{"center": Vector3(1, 0, 0), "radius": deg_to_rad(90)}],
-			"RightFoot":  [{"center": Vector3(1, 0, 0), "radius": deg_to_rad(90)}],
+			"LeftFoot":  [{"center": Vector3(0, -1, 0), "radius": deg_to_rad(20)}],
+			"RightFoot":  [{"center": Vector3(0, -1, 0), "radius": deg_to_rad(20)}],
+#			"LeftToes":  [{"center": Vector3(1, 0, 0), "radius": deg_to_rad(90)}],
+#			"RightToes":  [{"center": Vector3(1, 0, 0), "radius": deg_to_rad(90)}],
 		},
 	}
 
@@ -96,14 +117,22 @@ func _run():
 		if property["name"] == "update_in_editor":
 			root.set("update_in_editor", true)
 	var iks : Array[Node] = root.find_children("*", "ManyBoneIK3D")
-	for ik in iks:
-		for node in ik.get_node(ik.skeleton_node_path).get_children():
-			if node.name in targets:
-				node.free()
-		ik.free()
-	var new_ik : ManyBoneIK3D = ManyBoneIK3D.new()
+	var reniks : Array[Node] = root.find_children("*", "RenIK")
 	var skeletons : Array[Node] = root.find_children("*", "Skeleton3D")
 	var skeleton : Skeleton3D = skeletons[0]
+	for ik in iks:
+		var renik : RenIK = reniks[0]
+		for node in skeleton.get_children():
+			if str(node.name) in targets:
+				node.free()
+		renik.armature_head_target = "Head"
+		renik.armature_left_hand_target = "LeftHand"
+		renik.armature_right_hand_target = "RightHand"
+		renik.armature_hip_target = "Hips"
+		renik.armature_left_foot_target = "LeftFoot"
+		renik.armature_right_foot_target = "RightFoot"
+		ik.free()
+	var new_ik : ManyBoneIK3D = ManyBoneIK3D.new()
 	skeleton.add_child(new_ik, true)
 	new_ik.skeleton_node_path = ".."
 	new_ik.owner = root
@@ -111,33 +140,43 @@ func _run():
 	new_ik.default_damp = deg_to_rad(10)
 	new_ik.filter_bones = []
 	new_ik.queue_print_skeleton()
-#	new_ik.constraint_mode = true
+	new_ik.constraint_mode = true
 	skeleton.reset_bone_poses()
 	var humanoid_profile : SkeletonProfileHumanoid = SkeletonProfileHumanoid.new()
 	var humanoid_bones : PackedStringArray = []
 	for profile_i in humanoid_profile.bone_size:
 		var bone_name : String = humanoid_profile.get_bone_name(profile_i)
 		humanoid_bones.push_back(bone_name)
-	var is_humanoid : bool = false
-	var is_filtering : bool = true
-
+	
+	var skeleton_profile = SkeletonProfileHumanoid.new()
+	var human_bones : Array
+	for bone_i in skeleton_profile.bone_size:
+		var bone_name : String = skeleton_profile.get_bone_name(bone_i)
+		human_bones.push_back(bone_name)
 	new_ik.filter_bones.append_array(["LeftIndexProximal", "LeftLittleProximal", "LeftMiddleProximal", "LeftRingProximal", "LeftThumbMetacarpal",
 	"RightIndexProximal", "RightLittleProximal", "RightMiddleProximal", "RightRingProximal", "RightThumbMetacarpal",
 #			"LeftShoulder", "RightShoulder",
 #			"LeftUpperLeg", "LeftUpperLeg",
 	"RightEye", "LeftEye",
-	"RightToes", "LeftToes",
+#	"RightToes", "LeftToes",
 	])
 	var bone_name_from_to_twist = config["bone_name_from_to_twist"]
 	var bone_name_cones = config["bone_name_cones"]
 	for bone_i in skeleton.get_bone_count():
 		var bone_name : String = skeleton.get_bone_name(bone_i)
+		new_ik.set_kusudama_twist(bone_i, Vector2(0, TAU))
+		new_ik.set_pin_weight(bone_i, 0)
+		new_ik.set_kusudama_limit_cone_count(bone_i, 0)
+#		if not human_bones.has(bone_name):
+#			new_ik.filter_bones.push_back(bone_name)
 		var keys : Array = bone_name_from_to_twist.keys()
 		if keys.has(bone_name):
+			new_ik.set_pin_weight(bone_i, 1)
 			var twist : Vector2 = bone_name_from_to_twist[bone_name]
 			new_ik.set_kusudama_twist(bone_i, twist)
 		keys = bone_name_cones.keys()
 		if keys.has(bone_name):
+			new_ik.set_pin_weight(bone_i, 1)
 			var cones : Array = bone_name_cones[bone_name]
 			new_ik.set_kusudama_limit_cone_count(bone_i, cones.size())
 			for cone_i in range(cones.size()):
@@ -148,34 +187,31 @@ func _run():
 					new_ik.set_kusudama_limit_cone_radius(bone_i, cone_i, cone["radius"])
 		if not bone_name in targets:
 			continue
-		tune_bone(new_ik, skeleton, bone_i)
+		tune_bone(new_ik, reniks[0], skeleton, bone_i)
 
-func tune_bone(new_ik : ManyBoneIK3D, skeleton, bone_i):
-	var node_3d : BoneAttachment3D = BoneAttachment3D.new()
+func tune_bone(new_ik : ManyBoneIK3D, renik : RenIK, skeleton : Skeleton3D, bone_i : int):
+	var node_3d : Marker3D = Marker3D.new()
 	var bone_name = skeleton.get_bone_name(bone_i)
 	node_3d.name = bone_name
-	node_3d.bone_idx = bone_i
-	node_3d.set_use_external_skeleton (true)
-	node_3d.set_external_skeleton("../" + str(new_ik.get_path_to(skeleton)))
+	node_3d.transform = skeleton.get_bone_global_pose_no_override(bone_i)
 	new_ik.add_child(node_3d, true)
 	if bone_name in ["Root"]:
 		new_ik.set_pin_passthrough_factor(bone_i, 0)
+		new_ik.set_pin_weight(bone_i, 0)
+	if bone_name in ["Hips"]:
+		new_ik.set_pin_passthrough_factor(bone_i, 0)
+		new_ik.set_pin_weight(bone_i, 0)
 	if bone_name in ["Head"]:
 		# Move slightly higher to avoid the crunching into the body effect.
 		node_3d.transform.origin = node_3d.transform.origin + Vector3(0, 0.1, 0)
 		new_ik.set_pin_passthrough_factor(bone_i, 1)
 	if bone_name in ["LeftHand"]:
-		new_ik.set_pin_passthrough_factor(bone_i, 1)
+		new_ik.set_pin_passthrough_factor(bone_i, 0)
 	if bone_name in ["RightHand"]:
-		new_ik.set_pin_passthrough_factor(bone_i, 1)
+		new_ik.set_pin_passthrough_factor(bone_i, 0)
 	if bone_name in ["LeftFoot", "RightFoot"]:
+		new_ik.set_pin_passthrough_factor(bone_i, 0)
 		node_3d.global_transform.basis = Basis.from_euler(Vector3(0, PI, 0))
-		new_ik.set_pin_passthrough_factor(bone_i, 1)
 	node_3d.owner = new_ik.owner
-	new_ik.set_pin_nodepath(bone_i, str(NodePath(node_3d.name)))
-	var node_transform =  node_3d.global_transform
-	var marker_3d : Marker3D = Marker3D.new()
-	marker_3d.name = node_3d.name
-	marker_3d.global_transform = node_transform
-	node_3d.replace_by(marker_3d, true)
+	new_ik.set_pin_nodepath(bone_i, str(node_3d.name))
 
