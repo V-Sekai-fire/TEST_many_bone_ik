63c5760c144c5ad5ac2031c677ab268f7d5d1e63
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-07-19 11:28:32 -0700

    Clean up configure.gd and add VS Code config
    - Remove unused chamfer distance functions from configure.gd
    - Add VS Code settings for Godot 4 editor path

--- n_bone_ik/core/configure.gd
+++ n_bone_ik/core/configure.gd
@@ -7,37 +7,6 @@
 extends EditorScript
 
 
-func euclidean_distance(p1, p2):
-	return p1.distance_to(p2)
-
-func chamfer_distance(set_A, set_B):
-	var total_distance = 0.0
-
-	for point_A in set_A:
-		var min_distance = INF
-		for point_B in set_B:
-			var distance = euclidean_distance(point_A, point_B)
-			if distance < min_distance:
-				min_distance = distance
-		total_distance += min_distance
-
-	for point_B in set_B:
-		var min_distance = INF
-		for point_A in set_A:
-			var distance = euclidean_distance(point_B, point_A)
-			if distance < min_distance:
-				min_distance = distance
-		total_distance += min_distance
-
-	return total_distance / (set_A.size() + set_B.size())
-
-
-func test_chamfer_distance():
-	var set_A = [Vector2(1, 2), Vector2(3, 4), Vector2(5, 6)]
-	var set_B = [Vector2(7, 8), Vector2(9, 10), Vector2(11, 12)]
-	print(chamfer_distance(set_A, set_B))
-
-
 class LimitCone:
 	var direction: Vector3
 	var angle: float
